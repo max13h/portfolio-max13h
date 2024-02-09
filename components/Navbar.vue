c986d60d424488844c4bf52e092d18c2bc3c4853
max13h <contact@max13h.fr>
2024-02-09 22:17:46 +0100

    style contact button

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
-  <nav class="fixed w-full pt-4 pe-8 flex justify-end mix-blend-difference font-light text-light z-50">
-    <NuxtLink class="transition-all duration-700" @click="lenisStore.lenis.scrollTo('#Contact')">
+  <nav class="fixed w-full flex justify-end items-center mix-blend-difference font-light text-light z-50 ">
+    <NuxtLink class="transition-all duration-15000 hover:scale-105 hover:outline hover:outline-2 hover:outline-gray-400 py-2 px-4 rounded-2xl mt-4 me-4" @click="lenisStore.lenis.scrollTo('#Contact')">
       Contact
     </NuxtLink>
   </nav>
@@ -11,5 +11,4 @@ const lenisStore = useLenisStore()
 </script>
 
 <style scoped>
-
 </style>
